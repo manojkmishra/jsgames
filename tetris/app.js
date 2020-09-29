c0779ec0ed4648ef002c78945086c4b01e1d8e38
manojkmishra <manojmishra571980@gmail.com>
2020-09-29 23:38:30 +1000

    stop at bottom, new falling tetris

--- tetris/app.js
+++ tetris/app.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //console.log('score',scoreDisplay)
     //console.log('startBtn',startBtn)
 
-//The Tetrominoes--------------------
+//=========define--The Tetrominoes=================
   const width = 10
   const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
@@ -46,23 +46,46 @@ document.addEventListener('DOMContentLoaded', () => {
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3]
   ]
+//==================draw the tetominos and undraw========================
 //------------combile all tetromino-------------
     const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
     console.log('all theTetrominoes',theTetrominoes)
 //------------draw first roation of L tetromino-----ie add div with 4 values------
-let random = Math.floor(Math.random() * theTetrominoes.length)
-console.log('random',random);
-//console.log('L theTetrominoes',theTetrominoes[0][0]) //random-- 0-L,1-Z,2-T,3-O,4-I
 let currentPosition=4//start from fourth square
 let currentRotation=0 //always choose first rotation of all shapes 
-//let current=theTetrominoes[0][0] // L tetromino
-let current=theTetrominoes[random][currentRotation]
-console.log('random theTetrominoes',current) 
-function draw()
-{   current.forEach(index=>{squares[currentPosition+index].classList.add('tetromino')})}
-draw()
-function undraw()
-{   current.forEach(index=>{squares[currentPosition+index].classList.remove('tetromino')})}
-undraw()
+    let random = Math.floor(Math.random() * theTetrominoes.length)
+    console.log('random',random);
+    //console.log('L theTetrominoes',theTetrominoes[0][0]) //random-- 0-L,1-Z,2-T,3-O,4-I
+    //let current=theTetrominoes[0][0] // L tetromino
+    let current=theTetrominoes[random][currentRotation] //random-- 0-L,1-Z,2-T,3-O,4-I ; currentRotation=0 first one
+    console.log('current theTetrominoes',current) 
+    function draw()
+    {   current.forEach(index=>{squares[currentPosition+index].classList.add('tetromino')})}
+    //draw()
+    function undraw()
+    {   current.forEach(index=>{squares[currentPosition+index].classList.remove('tetromino')})}
+    //undraw()
+//================move down==========================================
+timerId=setInterval(moveDown,1000)
+    function moveDown(){
+        undraw()
+        currentPosition += width
+        draw()
+        console.log('movedown-currentpos=',currentPosition)
+        freeze()
+    }
+//-----------------freeze/stop falling tetrominos---------------------------
+function freeze() {
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) 
+    { console.log('freeze at bottom-,currentpos=',currentPosition)
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+      //start a new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length)
+      current = theTetrominoes[random][currentRotation]
+      currentPosition = 4
+      draw()
+    }
+  }
 
+  
 })
